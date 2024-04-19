537c9f7f47afdc31c7873c6aafc8e0748f7e5e87
junho0957 <juo0957@naver.com>
2024-04-19 18:52:40 +0900

    20232057 송준호

---
+++ chp6/Project1/Project1/6_2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include "Student_info.h"
+#include "analysis.h"
+
+using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int main()
+{
+    
+    vector<Student_info> did, didnt;
+
+    
+    Student_info student;
+   
+    while (read(cin, student)) {
+        if (did_all_hw(student))
+            did.push_back(student);
+        else
+            didnt.push_back(student);
+    }
+
+    // verify that the analyses will show us something
+    if (did.empty()) {
+        cout << "No student did all the homework!" << endl;
+        return 1;
+    }
+    if (didnt.empty()) {
+        cout << "Every student did all the homework!" << endl;
+        return 1;
+    }
+
+    // do the analyses
+    write_analysis(cout, "median", median_analysis, did, didnt);
+    write_analysis(cout, "average", average_analysis, did, didnt);
+    write_analysis(cout, "median of homework turned in",
+        optimistic_median_analysis, did, didnt);
+
+    return 0;
+}
